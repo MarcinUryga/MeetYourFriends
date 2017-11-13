96c09e82c9d3df7ed5c4d96a23086db366c633d4
Marcinos123 <marcin.uryga@onet.eu>
2017-11-14 00:47:51 +0100

    prepared frends fragment do display recyclerview

--- app/src/main/java/com/example/marcin/meetfriends/ui/venues/VenuesFragment.kt
+++ app/src/main/java/com/example/marcin/meetfriends/ui/venues/VenuesFragment.kt
@@ -2,6 +2,11 @@ package com.example.marcin.meetfriends.ui.venues
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marcin.meetfriends.R
 import com.example.marcin.meetfriends.mvp.BaseFragment
 import dagger.android.support.AndroidSupportInjection
 
@@ -14,5 +19,9 @@ class VenuesFragment : BaseFragment<VenuesContract.Presenter>(), VenuesContract.
   override fun onAttach(context: Context?) {
     AndroidSupportInjection.inject(this)
     super.onAttach(context)
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_venues, container, false)
   }
 }
