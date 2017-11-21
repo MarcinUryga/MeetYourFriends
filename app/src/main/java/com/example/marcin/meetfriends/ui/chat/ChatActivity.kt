336e25d075c401859ea08ecd7384d719bcaea9f4
marcin <marcin.uryga@onet.eu>
2017-11-21 13:32:46 +0100

    created layouts for chat activity

--- app/src/main/java/com/example/marcin/meetfriends/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/marcin/meetfriends/ui/chat/ChatActivity.kt
@@ -3,12 +3,12 @@ package com.example.marcin.meetfriends.ui.chat
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import com.example.marcin.meetfriends.R
 import com.example.marcin.meetfriends.models.Event
 import com.example.marcin.meetfriends.mvp.BaseActivity
 import com.example.marcin.meetfriends.utils.Constants
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_chat_room.*
 
 class ChatActivity : BaseActivity<ChatContract.Presenter>(), ChatContract.View {
 
@@ -17,7 +17,9 @@ class ChatActivity : BaseActivity<ChatContract.Presenter>(), ChatContract.View {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_chat_room)
     val event = intent.getSerializableExtra(Constants.CHAT_ROOM_EXTRA) as Event
-    Toast.makeText(baseContext, event.name, Toast.LENGTH_SHORT).show()
+    sendMessageButton.setOnClickListener{
+
+    }
   }
 
   companion object {
